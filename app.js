4b69ef95e1d800900684f2f8d1cb66732bc8bf6d
Juan Manuel <juanmanuel31@hotmail.com>
2025-04-13 00:00:44 +0200

    x-powered-by, false

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("x-powered-by", false);
+
 
 app.use(logger("dev"));
 app.use(express.json());
